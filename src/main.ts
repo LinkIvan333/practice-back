d5052aebc29aa46c77b5f541690b0276c795bc9c
19816388 <IYZamotaev@sberbank.ru>
2022-08-08 16:45:08 +0300

    add prefix api

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const config = new DocumentBuilder()
-      .setTitle('Cats example')
-      .setDescription('The cats API description')
+      .setTitle('Models')
+      .setDescription('Models online shop')
       .setVersion('1.0')
-      .addTag('cats')
+      .addTag('models')
       .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
@@ -21,6 +21,7 @@ async function bootstrap() {
     transform: true,
   }));
   app.enableCors()
+  app.setGlobalPrefix('api');
   app.useStaticAssets(join(__dirname, '..', 'static'), {
     prefix: '/static/'
   })
